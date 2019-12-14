8363600cd72a87ca3c8ab752f9dd1951ef31c9fd
tentone <tentone@outlook.com>
2019-12-14 18:02:22 +0000

    Test environment

--- src/main.py
+++ src/main.py
@@ -9,29 +9,17 @@ import cv2
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Kerasa / TensorFlow
 from keras.models import load_model
 from layers import BilinearUpSampling2D
 from loss import depth_loss_function
 from utils import predict, display_images, to_multichannel, scale_up
 from matplotlib import pyplot as plt
 
-# Argument Parser
-parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
-parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='test/*.mov', type=str, help='Path to Video')
-args = parser.parse_args()
-
-# Custom object needed for inference and training
-start = time.time()
-custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': depth_loss_function}
-
-print('Loading model...')
-
-# Load model into GPU / CPU
-model = load_model(args.model, custom_objects=custom_objects, compile=False)
-
-print('\nModel loaded ({0}).'.format(args.model))
+import roslib
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 def get_img_arr(image):
     im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -39,18 +27,9 @@ def get_img_arr(image):
     x = np.clip(np.asarray(im, dtype=float) / 255, 0, 1)
     return x
 
-video_name = args.input
-
-cap = cv2.VideoCapture(video_name)
-out_video_name = 'output.avi'
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-out = cv2.VideoWriter(out_video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (1280, 480))
-
 def display_single_image(output, inputs=None, is_colormap=True):
     import matplotlib.pyplot as plt
-
     plasma = plt.get_cmap('plasma')
-
     imgs = []
     imgs.append(inputs)
 
@@ -68,6 +47,26 @@ def display_single_image(output, inputs=None, is_colormap=True):
     img_set = np.hstack(imgs)
 
     return img_set
+
+
+# Argument Parser
+#parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
+#parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
+#parser.add_argument('--input', default='test/*.mov', type=str, help='Path to Video')
+#args = parser.parse_args()
+
+# Custom object needed for inference and training
+start = time.time()
+custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': depth_loss_function}
+
+# Load model into GPU / CPU
+model = load_model(args.model, custom_objects=custom_objects, compile=False)
+
+video_name = args.input
+cap = cv2.VideoCapture(video_name)
+out_video_name = 'output.avi'
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+out = cv2.VideoWriter(out_video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (1280, 480))
 
 count = 0
 ret = True
