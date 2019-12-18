f0df8d0e72749557e7b867c20b0fb419e96ffcbc
tentone <tentone@outlook.com>
2019-12-18 09:50:53 +0000

    Derp

--- src/monodepth.py
+++ src/monodepth.py
@@ -23,13 +23,15 @@ class MonoDepth():
         self.session.run(self.init)
 
         # Get parameters
-        self.topic_color = rospy.get_param('~topic_color', '/camera/image_raw')
-        self.topic_depth = rospy.get_param('~topic_depth', '/camera/image_depth')
-        self.topic_pointcloud = rospy.get_param('~topic_depth', '/pointcloud')
+        self.debug = rospy.get_param("~debug", True)
+        self.frame_id = rospy.get_param("~frame_id", "map")
+        self.topic_color = rospy.get_param("~topic_color", "/camera/image_raw")
+        self.topic_depth = rospy.get_param("~topic_depth", "/camera/image_depth")
+        self.topic_pointcloud = rospy.get_param("~topic_pointcloud", "/pointcloud")
 
         # Read keras model
         self.rospack = rospkg.RosPack()
-        self.model_path = self.rospack.get_path("monodepth") + "/models/kitti.h5"
+        self.model_path = self.rospack.get_path("monodepth") + "/models/nyu.h5"
 
         # Custom object needed for inference and training
         self.start = time.time()
@@ -39,50 +41,61 @@ class MonoDepth():
         self.model = keras.models.load_model(self.model_path, custom_objects=self.custom_objects, compile=False)
         self.model._make_predict_function()
 
-        # Image publisher
-        self.image_pub = rospy.Publisher(self.topic_depth, Image, queue_size=1)
+        # Publishers
+        self.pub_image_depth = rospy.Publisher(self.topic_depth, Image, queue_size=1)
+        self.pub_pointcloud = rospy.Publisher(self.topic_pointcloud, PointCloud2, queue_size=1)
+        self.counter = 0
 
-        # Image subscriber
+        # Subscribers
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber(self.topic_color, Image, self.image_callback)
+        self.sub_image_raw = rospy.Subscriber(self.topic_color, Image, self.image_callback)
 
-    # Create a sensor_msgs.PointCloud2 from an array of points.
-    def xyzrgb_array_to_pointcloud2(self, depth, color, stamp=None, frame_id=None, seq=None):
+    # Create a sensor_msgs.PointCloud2 from the depth and color images provided
+    #
+    # It ignores are camera parameters and assumes the images to be rectified
+    def create_pointcloud(self, depth, color):
         msg = PointCloud2()
-        buf = []
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = self.frame_id
+        msg.header.seq = self.counter
 
-        if stamp:
-            msg.header.stamp = stamp
-        if frame_id:
-            msg.header.frame_id = frame_id
-        if seq:
-            msg.header.seq = seq
+        depth_height, depth_width, depth_channels = depth.shape
+        color_height, color_width, color_channels = color.shape
 
-        height, width, channels = depth.shape
+        # Point cloud data numpy array
+        i = 0
+        data = np.zeros((depth_height * depth_width * 3), dtype=np.float32)
 
-        if len(points.shape) == 3:
-            msg.height = points.shape[1]
-            msg.width = points.shape[0]
-        else:
-            N = len(points)
-            xyzrgb = np.array(np.hstack([points, colors]), dtype=np.float32)
-            msg.height = 1
-            msg.width = N
+        # Message data size
+        msg.height = 1
+        msg.width = depth_width * depth_height
 
+        # Iterate images and build point cloud
+        for y in range(depth_height):
+            for x in range(depth_width):
+                #yc = y * depth_height / color_height
+                #xc = x * depth_width / color_width
+                data[i] =  x
+                data[i + 1] = y
+                data[i + 2] = depth[y, x]
+
+                i += 3
+
+        # Fields of the point cloud
         msg.fields = [
-            PointField('x', 0, PointField.FLOAT32, 1),
-            PointField('y', 4, PointField.FLOAT32, 1),
-            PointField('z', 8, PointField.FLOAT32, 1),
-            PointField('r', 12, PointField.FLOAT32, 1),
-            PointField('g', 16, PointField.FLOAT32, 1),
-            PointField('b', 20, PointField.FLOAT32, 1)
+            PointField("x", 0, PointField.FLOAT32, 1),
+            PointField("y", 4, PointField.FLOAT32, 1),
+            PointField("z", 8, PointField.FLOAT32, 1),
+            #PointField("r", 12, PointField.FLOAT32, 1),
+            #PointField("g", 16, PointField.FLOAT32, 1),
+            #PointField("b", 20, PointField.FLOAT32, 1)
         ]
 
         msg.is_bigendian = False
-        msg.point_step = 24
-        msg.row_step = msg.point_step * N
-        msg.is_dense = True;
-        msg.data = xyzrgb.tostring()
+        msg.point_step = 12
+        msg.row_step = msg.point_step * depth_height * depth_width
+        msg.is_dense = True
+        msg.data = data.tostring()
 
         return msg
 
@@ -97,13 +110,13 @@ class MonoDepth():
             print(e)
 
         # Display image
-        # cv2.imshow("Image", image)
-        # cv2.waitKey(1)
+        if self.debug:
+            cv2.imshow("Image", image)
+            cv2.waitKey(1)
 
         # Get image data as a numpy array to be passed for processing.
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # img = cv2.resize(img, (640, 480))
-        arr = np.clip(np.asarray(img, dtype=float) / 255, 0, 1)
+        arr = np.clip(np.asarray(img, dtype=np.float32) / 255, 0, 1)
 
         # Predict depth image
         with self.session.as_default():
@@ -114,14 +127,20 @@ class MonoDepth():
         depth = result.reshape(result.shape[1], result.shape[2], 1)
 
         # Display depth
-        # cv2.imshow("Result", depth)
-        # cv2.waitKey(1)
+        if self.debug:
+            cv2.imshow("Result", depth)
+            cv2.waitKey(1)
 
         # Publish depth image
         depth = 255 * depth
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(depth.astype(np.uint8), "mono8"))
+        self.pub_image_depth.publish(self.bridge.cv2_to_imgmsg(depth.astype(np.uint8), "mono8"))
 
-        #
+        # Generate Point cloud
+        cloud_msg = self.create_pointcloud(depth, image)
+        self.pub_pointcloud.publish(cloud_msg)
+
+        # Increment counter
+        self.counter += 1
 
 def main():
     rospy.init_node("monodepth")
